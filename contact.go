36171c5b10bc14e893380b360884354eb5074a0c
Artem Tiumentcev <darland.maik@gmail.com>
2019-10-18 12:33:28 +0300

    Add a new method GetAllContacts

--- contact.go
+++ contact.go
@@ -1,6 +1,9 @@
 package gohubspot
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
 type ContactsService service
 
@@ -15,61 +18,77 @@ type Contact struct {
 	Properties   Properties `json:"properties"`
 }
 
+type Contacts struct {
+	Contacts  []Contact `json:"contacts"`
+	HasMore   bool      `json:"has-more"`
+	VidOffset int       `json:"vid-offset"`
+}
+
 func (s *ContactsService) Create(properties Properties) (*IdentityProfile, error) {
-	url := "/contacts/v1/contact"
+	urlPath := "/contacts/v1/contact"
 	res := new(IdentityProfile)
-	err := s.client.RunPost(url, properties, res)
+	err := s.client.RunPost(urlPath, properties, res)
 	return res, err
 }
 
 func (s *ContactsService) Update(contactID int, properties Properties) error {
-	url := fmt.Sprintf("/contacts/v1/contact/vid/%d/profile", contactID)
-	return s.client.RunPost(url, properties, nil)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/vid/%d/profile", contactID)
+	return s.client.RunPost(urlPath, properties, nil)
 }
 
 func (s *ContactsService) UpdateByEmail(email string, properties Properties) error {
-	url := fmt.Sprintf("/contacts/v1/contact/email/%s/profile", email)
-	return s.client.RunPost(url, properties, nil)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/email/%s/profile", email)
+	return s.client.RunPost(urlPath, properties, nil)
 }
 
 func (s *ContactsService) CreateOrUpdateByEmail(email string, properties Properties) (*Vid, error) {
-	url := fmt.Sprintf("/contacts/v1/contact/createOrUpdate/email/%s", email)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/createOrUpdate/email/%s", email)
 
 	res := new(Vid)
-	err := s.client.RunPost(url, properties, res)
+	err := s.client.RunPost(urlPath, properties, res)
 	return res, err
 }
 
 func (s *ContactsService) DeleteById(id int) (*ContactDeleteResult, error) {
-	url := fmt.Sprintf("/contacts/v1/contact/vid/%d", id)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/vid/%d", id)
 
 	res := new(ContactDeleteResult)
-	err := s.client.RunDelete(url, res)
+	err := s.client.RunDelete(urlPath, res)
 	return res, err
 }
 
 func (s *ContactsService) DeleteByEmail(email string) (*ContactDeleteResult, error) {
-	url := fmt.Sprintf("/contacts/v1/contact/email/%s", email)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/email/%s", email)
 
 	res := new(ContactDeleteResult)
-	err := s.client.RunDelete(url, res)
+	err := s.client.RunDelete(urlPath, res)
 	return res, err
 }
 
 func (s *ContactsService) Merge(primaryID, secondaryID int) error {
-	url := fmt.Sprintf("/contacts/v1/contact/merge-vids/%d/", primaryID)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/merge-vids/%d/", primaryID)
 	secondary := struct {
 		SecondaryID int `json:"vidToMerge"`
 	}{
 		SecondaryID: secondaryID,
 	}
 
-	return s.client.RunPost(url, secondary, nil)
+	return s.client.RunPost(urlPath, secondary, nil)
 }
 
 func (s *ContactsService) GetByToken(token string) (*Contact, error) {
-	url := fmt.Sprintf("/contacts/v1/contact/utk/%s/profile", token)
+	urlPath := fmt.Sprintf("/contacts/v1/contact/utk/%s/profile", token)
 	res := new(Contact)
-	err := s.client.RunGet(url, res)
+	err := s.client.RunGet(urlPath, res)
+	return res, err
+}
+
+func (s *ContactsService) GetAllContacts(params *url.Values) (*Contacts, error) {
+	urlPath := "/contacts/v1/lists/all/contacts/all?"
+	if params != nil {
+		urlPath += params.Encode()
+	}
+	res := new(Contacts)
+	err := s.client.RunGet(urlPath, res)
 	return res, err
 }
